b073aed3d4b412af7bb859038dfb551837c32954
miaochaoqing <419666193@qq.com>
2018-04-21 21:40:07 +0800

    django中配置日志器的使用

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
+import logging
 from django.shortcuts import render
+
+logger = logging.getLogger("blog.views")
 
 # Create your views here.
 def index(request):
+    try:
+        file = open("sss.txt","r")
+    except Exception as e:
+        logger.error(e)
+
     return render(request,'index.html',locals())
