0fbe6d3a9607f914391b2c4abde4d80207d81258
miaochaoqing <419666193@qq.com>
2018-04-23 20:19:48 +0800

    admin 里的副文本,引用第三方模块:kindeditor

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from blog.models import *
 
 # Register your models here.
+class AtrticleAdmin(admin.ModelAdmin):
+
+    # 只显示这些内容
+    # fields = ("title","desc","content")
+    # 除开这些内容外
+    # exclude = ("title","desc","content")
+
+    class Media:
+        js = (
+            '/static/js/kindeditor-4.1.10/kindeditor-min.js',
+            '/static/js/kindeditor-4.1.10/lang/zh_CN.js',
+            '/static/js/kindeditor-4.1.10/config.js',
+        )
+
+
+admin.site.register(User)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Article,AtrticleAdmin)
+admin.site.register(Comment)
+admin.site.register(Links)
+admin.site.register(Ad)
